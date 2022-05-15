012b9e27a882dce218167308de39e464931ab8f5
MangKyu <whalsrb1226@gmail.com>
2022-05-15 16:51:05 +0900

    RequestBodyControllerAdvice를 사용하도록 수정

---
+++ src/main/java/com/mangkyu/requestbody/app/RequestBodyControllerAdvice.java
@@ -0,0 +1,37 @@
+package com.mangkyu.requestbody.app;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+@RestControllerAdvice
+public class RequestBodyControllerAdvice implements RequestBodyAdvice {
+
+    @Override
+    public boolean supports(final MethodParameter methodParameter, final Type targetType, final Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public HttpInputMessage beforeBodyRead(final HttpInputMessage inputMessage, final MethodParameter parameter, final Type targetType, final Class<? extends HttpMessageConverter<?>> converterType) {
+        return inputMessage;
+    }
+
+    @Override
+    public Object afterBodyRead(final Object body, final HttpInputMessage inputMessage, final MethodParameter parameter, final Type targetType, final Class<? extends HttpMessageConverter<?>> converterType) {
+        final User user = (User) body;
+        user.setDesc("desc");
+        return user;
+    }
+
+    @Override
+    public Object handleEmptyBody(final Object body, final HttpInputMessage inputMessage, final MethodParameter parameter, final Type targetType, final Class<? extends HttpMessageConverter<?>> converterType) {
+        return body;
+    }
+
+}
--- src/main/java/com/mangkyu/requestbody/app/UserController.java
+++ src/main/java/com/mangkyu/requestbody/app/UserController.java
@@ -2,6 +2,7 @@ package com.mangkyu.requestbody.app;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -10,7 +11,7 @@ import javax.validation.Valid;
 public class UserController {
 
     @PostMapping("/users")
-    public ResponseEntity<User> users(@DefaultUserRequestBody @Valid User user) {
+    public ResponseEntity<User> users(@RequestBody @Valid User user) {
         return ResponseEntity.ok(user);
     }
 
